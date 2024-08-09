da95bba8645ef7647d3566178d9b8daffbc5bbfd
Pawan9856 <pawanpaliwal4444@gmail.com>
2024-08-10 00:12:59 +0530

    adding empty chat container into application ui is done for empty chat container

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -1,22 +1,27 @@
-import { useAppStore } from '@/store'
-import React from 'react'
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-import { toast } from 'sonner';
+import { useAppStore } from "@/store";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import ContactsContainer from "./components/contacts-container";
+import EmptyChatContainer from "./components/empty-chat-container";
+import ChatContainer from "./components/chat-container";
 
 const Chat = () => {
-  const {userInfo}= useAppStore();
-  const navigate= useNavigate();
-  useEffect(()=>{
-    if(!userInfo.profileSetup){
+  const { userInfo } = useAppStore();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!userInfo.profileSetup) {
       toast("Plese Setup Profile To Continue.");
       navigate("/profile");
     }
-  },[userInfo,navigate]);
+  }, [userInfo, navigate]);
   return (
-    <div>Chat</div>
-  )
-}
+    <div className="flex text-white h-[100vh] overflow-hidden">
+      <ContactsContainer />
+      <EmptyChatContainer />
+      <ChatContainer />
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
